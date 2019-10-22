90dc89eba52be620334323833fde281c60fb792e
root <christopher.law89@gmail.com>
2019-10-22 05:33:18 +0000

    First Commit

---
+++ utils.py
@@ -0,0 +1,76 @@
+import nltk
+import pickle
+import re
+import numpy as np
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# Paths for all resources for the bot.
+RESOURCE_PATH = {
+    'INTENT_RECOGNIZER': 'intent_recognizer.pkl',
+    'TAG_CLASSIFIER': 'tag_classifier.pkl',
+    'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
+    'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
+    'WORD_EMBEDDINGS': 'word_embeddings.tsv',
+}
+
+
+def text_prepare(text):
+    """Performs tokenization and simple preprocessing."""
+    
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = bad_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+
+    return text.strip()
+
+
+def load_embeddings(embeddings_path):
+    """Loads pre-trained word embeddings from tsv file.
+
+    Args:
+      embeddings_path - path to the embeddings file.
+
+    Returns:
+      embeddings - dict mapping words to vectors;
+      embeddings_dim - dimension of the vectors.
+    """
+    
+    # Hint: you have already implemented a similar routine in the 3rd assignment.
+    # Note that here you also need to know the dimension of the loaded embeddings.
+    # When you load the embeddings, use numpy.float32 type as dtype
+
+    embeddings = {}
+    with open(embeddings_path, 'r') as file:
+        for line in file.readlines():
+            line_content = line[:-1].split('\t')
+            embeddings[line_content[0]] = np.array(line_content[1:], dtype=np.float32)
+
+    embeddings_dim = list(embeddings.values())[0].shape[0]
+
+    return embeddings, embeddings_dim
+
+
+def question_to_vec(question, embeddings, dim):
+    """Transforms a string to an embedding by averaging word embeddings."""
+    
+    # Hint: you have already implemented exactly this function in the 3rd assignment.
+
+    words = question.split()
+    num_words = len([word for word in words if word in embeddings.keys()])
+    if num_words == 0:
+        return np.zeros(dim)
+    else:
+        return np.sum([np.zeros(dim)] + [embeddings[word] for word in words if word in embeddings.keys()], axis=0) / num_words
+
+
+def unpickle_file(filename):
+    """Returns the result of unpickling the file content."""
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
